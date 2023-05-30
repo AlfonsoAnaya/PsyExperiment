c53cf600681a2dbe3a71a16c147f6eb4322f7daa
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-05-30 13:17:29 +0200

    set up form and php procesing individual data

--- process-form.php
+++ process-form.php
@@ -1,3 +1,14 @@
 <?php
 
-print_r($_POST);
+$participantid = $_POST["participantid"];
+$age = $_POST["age"];
+$gender = $_POST["gender"];
+$education =$_POST["education"];
+$responsetime=$_POST["responsetime"];
+$response = $_POST["response"];
+
+var_dump($participantid, $age, $gender, $education, $responsetime, $response)
+
+
+//print_r($_POST);
+?>
